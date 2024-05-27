de4ce0170a280ae1564dba9268f2ae5e2726e80c
David E. Perez Negron R. <david@nethunters.xyz>
2024-05-26 22:26:19 -0600

    local comunication working

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,16 @@ import getNodeOperatorAddress from './getNodeAddrs.js'
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+/* ToDO
+ [x] test conecting between nodes
+ [x] Make libp2p por static
+ [ ] make peerID static or import
+ [ ] setup dns resolver
+ [ ] Ethers-js import wallet from .env
+ [ ] Test with contracts
+ [ ] restructure code and create CLI
+ [ ] Daemon Setup
+*/
 
 async function main() {
     
@@ -29,29 +39,42 @@ async function main() {
     console.info('Helia is running');
     console.info('PeerId:', ipfs.libp2p.peerId.toString())
 
-    const peers = ipfs.libp2p.getPeers()
+    const addr = ipfs.libp2p.getMultiaddrs()
+    console.log(addr)
 
-    console.log(peers)
 
-
-    // TODO dial libp2p peerIDs
+    // TODO dial dns peerIDs
     //let node0 = "/dns4/ipfs.decentralizedscience.org/tcp/4004/ws/p2p/12D3KooWQzickgUJ1N9dNZMJpNnFUCHmhndTVgexXnt6dQhPFcEE";
     //let node0 = "/dnsaddr/ipfs.decentralizedscience.org/tcp/4004/ws/p2p/12D3KooWQzickgUJ1N9dNZMJpNnFUCHmhndTVgexXnt6dQhPFcEE";
     //let node0 = "/dns4/ipfs.decentralizedscience.org/tcp/443/ws/p2p/12D3KooWQzickgUJ1N9dNZMJpNnFUCHmhndTVgexXnt6dQhPFcEE";
     //let node0 = "/dnsaddr/ipfs.decentralizedscience.org/tcp/443/wss/p2p/12D3KooWQzickgUJ1N9dNZMJpNnFUCHmhndTVgexXnt6dQhPFcEE";
     //let node0 = "/dnsaddr/ipfs.decentralizedscience.org/p2p/12D3KooWQzickgUJ1N9dNZMJpNnFUCHmhndTVgexXnt6dQhPFcEE"
-    //
-    //await ipfs.libp2p.dial(multiaddr(node0))
+    
     //let node0 = "/dns4/ipfs.decentralizedscience.org/tcp/443/wss/p2p/";
-    //const addr = ipfs1.libp2p.getMultiaddrs()
-    //console.log(peer)
+    
+    // local test working
+    let node1 = multiaddr("/ip4/10.224.190.150/tcp/8080"); 
+    let node2 = multiaddr("/ip4/10.224.190.9/tcp/8080"); 
+    let peers = ipfs.libp2p.getPeers()
+
+    while(peers.length < 1) {
+      peers = ipfs.libp2p.getPeers()
+      try {
+        await ipfs.libp2p.dial(node1);
+        await ipfs.libp2p.dial(node2);
+      } catch (error) {
+	  console.log(error);
+      }
+    }
+
+    console.log(peers)
 
     //const peer = await ipfs.libp2p.peerStore.get(ipfs.libp2p.dial)
     //const peer = await ipfs.libp2p.peerStore.get(ipfs.libp2p.dial)
     //await ipfs.libp2p.dial(node0);
     //await ipfs.libp2p.hangUp(node0_ma)
     //await ipfs.libp2p.hangUp(node1_ma)
-    await ipfs.stop()
+    //await ipfs.stop()
 }
 
 main();
